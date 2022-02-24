75518cd8c711e7496d09e1ea72d514722c82e70c
Jayerson <ayersonj@gmail.com>
2022-02-24 15:35:39 +1100

    fast orbit calc

--- Simple Orbit/Orbit.js
+++ Simple Orbit/Orbit.js
@@ -17,25 +17,40 @@ const delay = (ms) => {
 }
 
 const variableSpeed = async () => {
-    for (let degrees = 0; degrees < 360; degrees++) {
-        let dist_angle = 
-            semi_maj * (1 - eccentricity ** 2) /
-            (1 - eccentricity * Math.cos(degrees/360*2*Math.PI) - Math.PI/2);
-        let segment_speed = Math.sqrt(2/dist_angle - 1/semi_maj);
-        let segment_time = Math.round(1/segment_speed);
-        orbits.ellipse(centroid_x, centroid_y, semi_maj, semi_min, 3*Math.PI/2, 
-            degrees/360*2*Math.PI, (degrees + 1)/360*2*Math.PI);
-        let x = await delay(segment_time);
-        console.log(x);
+    const start = Date.now();
+    
+    for (let degrees = 0; degrees < 360; degrees+=5) {
+        let startloop = Date.now()/1000;
+
+        segment_time(degrees);
+
+        orbits.ellipse(
+            centroid_x,
+            centroid_y,
+            semi_maj,
+            semi_min,
+            3*Math.PI/2, 
+            degrees/360*2*Math.PI,
+            (degrees + 5)/360*2*Math.PI);
+
+        await delay(segment_time);
         orbits.stroke();
+        
+        let endloop = Date.now()/1000;
+        console.log(endloop - startloop);
     }
+    console.log((Date.now() - start)/1000);
+}
+
+function segment_time(degrees) {
+    let dist_angle = semi_maj * (1 - eccentricity ** 2) /
+        (1 - eccentricity * Math.cos(degrees/360*2*Math.PI) - Math.PI/2);
+    let segment_speed = Math.sqrt(Math.abs(2/dist_angle - 1/semi_maj));
+    return Math.round(10/segment_speed);
 }
 
 orbits.beginPath();
 orbits.moveTo(centroid_x + 5, focus);
 orbits.arc(centroid_x, focus, 5, 0, 2*Math.PI);
-// x, y, x (major) radius, y (minor) radius, rot@, start@, end@
 orbits.moveTo(centroid_x, centroid_y - 120);
 variableSpeed();
-orbits.stroke();
-
